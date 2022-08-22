3cbf7d3eb5847a0213113c093163b5e48b558144
91820 <91820@DESKTOP-BOGI0C2>
2022-08-22 18:46:42 +0530

    commit

--- src/Componets/Works/Works.js
+++ src/Componets/Works/Works.js
@@ -1,8 +1,50 @@
 import React from 'react'
+import './Works.css';
+import Upwork from '../../img/Upwork.png';
+import Fiverr from '../../img/fiverr.png';
+import Amazon from '../../img/amazon.png';
+import Shopify from '../../img/Shopify.png';
+import Facebook from '../../img/Facebook.png';
 
 const Works = () => {
   return (
-    <div>Works</div>
+    <div className="works">
+         <div className="awesome">
+            <span>Works for All these</span>
+            <span>Branded & Clients</span>
+            <span>Lorem Ipsum is simply dummy text of the printing and typesetting industry.             
+            <br/>
+            Lorem Ipsum is simply dummy text of the printing
+            <br/>
+            Lorem Ipsum is simply dummy text of the printing
+            <br/>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry.     
+            </span>
+            <button className="button s-button">Hire Me</button>
+            <div className="blur s-blur1" style={{background:'#ABF1FF94'}}></div>
+        </div>        
+        <div className="w-right">
+            <div className="w-mainCircle">
+                <div className="w-secCircle">
+                    <img src={Upwork} alt="Upwork"/>
+                </div>
+                <div className="w-secCircle">
+                    <img src={Fiverr} alt="Upwork"/>
+                </div>
+                <div className="w-secCircle">
+                    <img src={Amazon} alt="Upwork"/>
+                </div>
+                <div className="w-secCircle">
+                    <img src={Shopify} alt="Upwork"/>
+                </div>
+                <div className="w-secCircle">
+                    <img src={Facebook} alt="Upwork"/>
+                </div>
+            </div>
+            <div className="w-backCircle blueCircle"></div>
+            <div className="w-backCircle yellowCircle"></div>
+        </div>
+    </div>
   )
 }
 
